bfe5d58d3860e6d0595ca407b7c4ccd80d5d7f16
Saleh Shakib <55012686+salehshakib@users.noreply.github.com>
2022-10-26 01:05:12 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+
+# Learning Lab Server Side
+
+This is learning lab server git repo.
+
+## Description
+
+- There are 2 api used here.
+- The server side is configured with express js
+- Firebase config key is hidden
+- nodemon, node cors
+- vercel deploy
+
+## Tech Stack
+
+**Server:** Node, Express, NodeMon, NodeCors, Vercel.
+
---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/categories.json');
+const courses = require('./data/courses.json');
+
+app.get('/', (req, res) => {
+    res.send('Courses API Running');
+});
+
+app.get('/course-categories', (req, res) => {
+    res.send(categories);
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(courses);
+    }
+    else {
+        const category_courses = courses.filter(n => n.category_id === id);
+        res.send(category_courses);
+    }
+});
+
+
+app.listen(port, () => {
+    console.log('Learning Lab Server running on port', port);
+})
